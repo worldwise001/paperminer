49e948130a515b9aa190efbb62fdd945d33b42c1
Sarah Harvey <s@shh.sh>
2019-12-31 20:38:37 -0800

    Some code cleanup

--- paperminer/converter.py
+++ paperminer/converter.py
@@ -1,26 +1,22 @@
-
-##  PDFLayoutAnalyzer
-##
 import logging
 import sys
-import traceback
 
 import six
 from pdfminer import utils
-from pdfminer.layout import LTPage, LTFigure, LTImage, LTLine, LTRect, LTCurve, LTChar, LTTextLineHorizontal, \
-    LTContainer, LTAnno, LAParams
-from pdfminer.pdfdevice import PDFTextDevice
+from pdfminer.converter import PDFLayoutAnalyzer
+from pdfminer.layout import LAParams, LTAnno, LTTextLineHorizontal, LTContainer
 from pdfminer.pdffont import PDFUnicodeNotDefined
-from pdfminer.utils import apply_matrix_pt, mult_matrix
+from pdfminer.utils import apply_matrix_pt
+
+from paperminer.layout import LTPageExtended, LTCharExtended, LTTextLineHorizontalExtended
 
 log = logging.getLogger(__name__)
 
-class PDFLayoutAnalyzer(PDFTextDevice):
+
+class PaperLayoutAnalyzer(PDFLayoutAnalyzer):
 
     def __init__(self, rsrcmgr, pageno=1, laparams=None):
-        PDFTextDevice.__init__(self, rsrcmgr)
-        self.pageno = pageno
-        self.laparams = laparams
+        super().__init__(rsrcmgr, pageno, laparams)
         self._stack = []
         self.cur_item = None
         self.printed = 0
@@ -30,73 +26,17 @@ class PDFLayoutAnalyzer(PDFTextDevice):
         (x0, y0, x1, y1) = page.mediabox
         (x0, y0) = apply_matrix_pt(ctm, (x0, y0))
         (x1, y1) = apply_matrix_pt(ctm, (x1, y1))
-        mediabox = (0, 0, abs(x0-x1), abs(y0-y1))
-        self.cur_item = LTPage(self.pageno, mediabox)
+        mediabox = (0, 0, abs(x0 - x1), abs(y0 - y1))
+        self.cur_item = LTPageExtended(self.pageno, mediabox)
         return
 
     def end_page(self, page):
         assert not self._stack, str(len(self._stack))
-        assert isinstance(self.cur_item, LTPage), str(type(self.cur_item))
+        assert isinstance(self.cur_item, LTPageExtended), str(type(self.cur_item))
         if self.laparams is not None:
             self.cur_item.analyze(self.laparams)
         self.pageno += 1
         self.receive_layout(self.cur_item)
-        return
-
-    def begin_figure(self, name, bbox, matrix):
-        self._stack.append(self.cur_item)
-        self.cur_item = LTFigure(name, bbox, mult_matrix(matrix, self.ctm))
-        return
-
-    def end_figure(self, _):
-        fig = self.cur_item
-        assert isinstance(self.cur_item, LTFigure), str(type(self.cur_item))
-        self.cur_item = self._stack.pop()
-        self.cur_item.add(fig)
-        return
-
-    def render_image(self, name, stream):
-        assert isinstance(self.cur_item, LTFigure), str(type(self.cur_item))
-        item = LTImage(name, stream,
-                       (self.cur_item.x0, self.cur_item.y0,
-                        self.cur_item.x1, self.cur_item.y1))
-        self.cur_item.add(item)
-        return
-
-    def paint_path(self, gstate, stroke, fill, evenodd, path):
-        shape = ''.join(x[0] for x in path)
-        if shape == 'ml':
-            # horizontal/vertical line
-            (_, x0, y0) = path[0]
-            (_, x1, y1) = path[1]
-            (x0, y0) = apply_matrix_pt(self.ctm, (x0, y0))
-            (x1, y1) = apply_matrix_pt(self.ctm, (x1, y1))
-            if x0 == x1 or y0 == y1:
-                self.cur_item.add(LTLine(gstate.linewidth, (x0, y0), (x1, y1),
-                    stroke, fill, evenodd, gstate.scolor, gstate.ncolor))
-                return
-        if shape == 'mlllh':
-            # rectangle
-            (_, x0, y0) = path[0]
-            (_, x1, y1) = path[1]
-            (_, x2, y2) = path[2]
-            (_, x3, y3) = path[3]
-            (x0, y0) = apply_matrix_pt(self.ctm, (x0, y0))
-            (x1, y1) = apply_matrix_pt(self.ctm, (x1, y1))
-            (x2, y2) = apply_matrix_pt(self.ctm, (x2, y2))
-            (x3, y3) = apply_matrix_pt(self.ctm, (x3, y3))
-            if ((x0 == x1 and y1 == y2 and x2 == x3 and y3 == y0) or
-                (y0 == y1 and x1 == x2 and y2 == y3 and x3 == x0)):
-                self.cur_item.add(LTRect(gstate.linewidth, (x0, y0, x2, y2),
-                    stroke, fill, evenodd, gstate.scolor, gstate.ncolor))
-                return
-        # other shapes
-        pts = []
-        for p in path:
-            for i in range(1, len(p), 2):
-                pts.append(apply_matrix_pt(self.ctm, (p[i], p[i+1])))
-        self.cur_item.add(LTCurve(gstate.linewidth, pts, stroke, fill,
-            evenodd, gstate.scolor, gstate.ncolor))
         return
 
     def render_char(self, matrix, font, fontsize, scaling, rise, cid, ncs, graphicstate):
@@ -110,7 +50,7 @@ class PDFLayoutAnalyzer(PDFTextDevice):
             text = self.handle_undefined_char(font, cid)
         textwidth = font.char_width(cid)
         textdisp = font.char_disp(cid)
-        item = LTChar(matrix, font, fontsize, scaling, rise, text, textwidth, textdisp, ncs, graphicstate)
+        item = LTCharExtended(matrix, font, fontsize, scaling, rise, text, textwidth, textdisp, ncs, graphicstate)
         self.cur_item.add(item)
         return item.adv
 
@@ -121,22 +61,22 @@ class PDFLayoutAnalyzer(PDFTextDevice):
     def receive_layout(self, ltpage):
         return
 
-class TestConverter(PDFLayoutAnalyzer):
+
+class PaperToTextConverter(PaperLayoutAnalyzer):
     def __init__(self, rsrcmgr, pageno=1):
         laparams = LAParams()
         for param in ("all_texts", "detect_vertical", "word_margin", "char_margin", "line_margin", "boxes_flow"):
             paramv = locals().get(param, None)
             if paramv is not None:
                 setattr(laparams, param, paramv)
-        PDFLayoutAnalyzer.__init__(self, rsrcmgr, pageno=pageno, laparams=laparams)
+        PaperLayoutAnalyzer.__init__(self, rsrcmgr, pageno=pageno, laparams=laparams)
         return
 
     @staticmethod
     def write_text(text):
         text = utils.compatible_encode_method(text, 'utf-8', 'ignore')
-        #if six.PY3:
-        #    text = text.encode()
-        # sys.stdout.write(text)
+        text = text.encode()
+        sys.stdout.write(text)
         return
 
     # a typical page is 612 x 792 in pts or 8.5 x 11 in inches
@@ -146,28 +86,16 @@ class TestConverter(PDFLayoutAnalyzer):
     # So that is 0 - [198-396] pts
     def receive_layout(self, ltpage):
         def render(item, parent, level):
-            if not isinstance(item, LTChar) and not isinstance(item, LTAnno):
-                sparent = parent.__class__.__name__ or 'root'
-                print(f'{"".join([" "] * level)} -> {item.__class__.__name__} {item.y1 if isinstance(item, LTPage) else ""}')
-            if isinstance(item, LTTextLineHorizontal):
-                self.write_text(f'{"".join([" "] * level)}      ')
-                print(f'{"".join([" "] * level)}      ' + str(item.x0))
+            if not isinstance(item, LTCharExtended) and not isinstance(item, LTAnno):
+                print(
+                    f'{"".join([" "] * level)} -> {item.__class__.__name__} {item.y1 if isinstance(item, LTPageExtended) else ""}')
+            if isinstance(item, LTTextLineHorizontalExtended):
+                print(f'{"".join([" "] * level)}      {item.fontsize} {item.get_text()}')
                 for child in item:
                     self.write_text(child.get_text())
             elif isinstance(item, LTContainer):
                 for child in item:
                     render(child, item, level + 1)
-            '''
-            if isinstance(item, LTChar) or isinstance(item, LTAnno):
-                self.write_text(item.get_text())
-            elif isinstance(item, LTTextLineHorizontal):
-                child = list(item)[0]
-                self.write_text(child.fontname + ' ' + '%0.5f' % child.size + '\n') # font
-            elif isinstance(item, LTTextBoxHorizontal):
-                self.write_text('\n')
-            elif isinstance(item, LTImage):
-                # do image stuff
-                pass
-            '''
+
         render(ltpage, None, 0)
         return
--- paperminer/layout.py
+++ paperminer/layout.py
@@ -1,17 +1,12 @@
-from pdfminer.layout import LTContainer, LTTextLineHorizontal, LTTextLineVertical, LTTextContainer, LTAnno, LTChar, \
-    IndexAssigner, LTTextBoxVertical, LTTextGroupTBRL, LTTextGroupLRTB, LTTextBoxHorizontal
-from pdfminer.utils import bbox2str, get_bound, apply_matrix_pt, matrix2str, fsplit, Plane, uniq
-
-from sortedcontainers import SortedListWithKey
+from pdfminer.layout import LTContainer, LTTextLineHorizontal, LTTextLineVertical, LTTextContainer, LTAnno, \
+    LTChar, LTLayoutContainer
+from pdfminer.utils import bbox2str
 
 
-##  LTLayoutContainer
-##
-class LTLayoutContainer(LTContainer):
+class LTLayoutContainerExtended(LTLayoutContainer):
 
     def __init__(self, bbox):
-        LTContainer.__init__(self, bbox)
-        self.groups = None
+        super().__init__(bbox)
         return
 
     # group_objects: group text object to textlines.
@@ -59,8 +54,8 @@ class LTLayoutContainer(LTContainer):
                           (obj0.vdistance(obj1) <
                            max(obj0.height, obj1.height) * laparams.char_margin))
 
-                if ((halign and isinstance(line, LTTextLineHorizontal)) or
-                    (valign and isinstance(line, LTTextLineVertical))):
+                if ((halign and isinstance(line, LTTextLineHorizontalExtended)) or
+                        (valign and isinstance(line, LTTextLineVertical))):
                     line.add(obj1)
                 elif line is not None:
                     yield line
@@ -71,181 +66,36 @@ class LTLayoutContainer(LTContainer):
                         line.add(obj0)
                         line.add(obj1)
                     elif halign and not valign:
-                        line = LTTextLineHorizontal(laparams.word_margin)
+                        line = LTTextLineHorizontalExtended(laparams.word_margin)
                         line.add(obj0)
                         line.add(obj1)
                     else:
-                        line = LTTextLineHorizontal(laparams.word_margin)
+                        line = LTTextLineHorizontalExtended(laparams.word_margin)
                         line.add(obj0)
                         yield line
                         line = None
             obj0 = obj1
         if line is None:
-            line = LTTextLineHorizontal(laparams.word_margin)
+            line = LTTextLineHorizontalExtended(laparams.word_margin)
             line.add(obj0)
         yield line
         return
 
-    # group_textlines: group neighboring lines to textboxes.
-    def group_textlines(self, laparams, lines):
-        plane = Plane(self.bbox)
-        plane.extend(lines)
-        boxes = {}
-        for line in lines:
-            neighbors = line.find_neighbors(plane, laparams.line_margin)
-            if line not in neighbors: continue
-            members = []
-            for obj1 in neighbors:
-                members.append(obj1)
-                if obj1 in boxes:
-                    members.extend(boxes.pop(obj1))
-            if isinstance(line, LTTextLineHorizontal):
-                box = LTTextBoxHorizontal()
-            else:
-                box = LTTextBoxVertical()
-            for obj in uniq(members):
-                box.add(obj)
-                boxes[obj] = box
-        done = set()
-        for line in lines:
-            if line not in boxes: continue
-            box = boxes[line]
-            if box in done:
-                continue
-            done.add(box)
-            if not box.is_empty():
-                yield box
-        return
 
-    # group_textboxes: group textboxes hierarchically.
-    def group_textboxes(self, laparams, boxes):
-        assert boxes, str((laparams, boxes))
+class LTCharExtended(LTChar):
+    """Actual letter in the text as a Unicode string."""
 
-        def dist(obj1, obj2):
-            """A distance function between two TextBoxes.
-
-            Consider the bounding rectangle for obj1 and obj2.
-            Return its area less the areas of obj1 and obj2,
-            shown as 'www' below. This value may be negative.
-                    +------+..........+ (x1, y1)
-                    | obj1 |wwwwwwwwww:
-                    +------+www+------+
-                    :wwwwwwwwww| obj2 |
-            (x0, y0) +..........+------+
-            """
-            x0 = min(obj1.x0, obj2.x0)
-            y0 = min(obj1.y0, obj2.y0)
-            x1 = max(obj1.x1, obj2.x1)
-            y1 = max(obj1.y1, obj2.y1)
-            return ((x1-x0)*(y1-y0) - obj1.width*obj1.height - obj2.width*obj2.height)
-
-        def isany(obj1, obj2):
-            """Check if there's any other object between obj1 and obj2.
-            """
-            x0 = min(obj1.x0, obj2.x0)
-            y0 = min(obj1.y0, obj2.y0)
-            x1 = max(obj1.x1, obj2.x1)
-            y1 = max(obj1.y1, obj2.y1)
-            objs = set(plane.find((x0, y0, x1, y1)))
-            return objs.difference((obj1, obj2))
-
-        def key_obj(t):
-            (c,d,_,_) = t
-            return (c,d)
-
-        dists = SortedListWithKey(key=key_obj)
-        for i in range(len(boxes)):
-            obj1 = boxes[i]
-            for j in range(i+1, len(boxes)):
-                obj2 = boxes[j]
-                dists.add((0, dist(obj1, obj2), obj1, obj2))
-        plane = Plane(self.bbox)
-        plane.extend(boxes)
-        while dists:
-            (c, d, obj1, obj2) = dists.pop(0)
-            if c == 0 and isany(obj1, obj2):
-                dists.add((1, d, obj1, obj2))
-                continue
-            if (isinstance(obj1, (LTTextBoxVertical, LTTextGroupTBRL)) or
-                isinstance(obj2, (LTTextBoxVertical, LTTextGroupTBRL))):
-                group = LTTextGroupTBRL([obj1, obj2])
-            else:
-                group = LTTextGroupLRTB([obj1, obj2])
-            plane.remove(obj1)
-            plane.remove(obj2)
-            removed = [obj1, obj2]
-            to_remove = [ (c,d,obj1,obj2) for (c,d,obj1,obj2) in dists
-                      if (obj1 in removed or obj2 in removed) ]
-            for r in to_remove:
-                dists.remove(r)
-            for other in plane:
-                dists.add((0, dist(group, other), group, other))
-            plane.add(group)
-        assert len(plane) == 1, str(len(plane))
-        return list(plane)
-
-    def analyze(self, laparams):
-        # textobjs is a list of LTChar objects, i.e.
-        # it has all the individual characters in the page.
-        (textobjs, otherobjs) = fsplit(lambda obj: isinstance(obj, LTChar), self)
-        for obj in otherobjs:
-            obj.analyze(laparams)
-        if not textobjs:
-            return
-        textlines = list(self.group_objects(laparams, textobjs))
-        (empties, textlines) = fsplit(lambda obj: obj.is_empty(), textlines)
-        for obj in empties:
-            obj.analyze(laparams)
-        textboxes = list(self.group_textlines(laparams, textlines))
-        if -1 <= laparams.boxes_flow and laparams.boxes_flow <= +1 and textboxes:
-            self.groups = self.group_textboxes(laparams, textboxes)
-            assigner = IndexAssigner()
-            for group in self.groups:
-                group.analyze(laparams)
-                assigner.run(group)
-            textboxes.sort(key=lambda box: box.index)
-        else:
-            def getkey(box):
-                if isinstance(box, LTTextBoxVertical):
-                    return (0, -box.x1, box.y0)
-                else:
-                    return (1, box.y0, box.x0)
-            textboxes.sort(key=getkey)
-        self._objs = textboxes + otherobjs + empties
-        return
+    def __init__(self, matrix, font, fontsize, scaling, rise, text, textwidth, textdisp, ncs, graphicstate):
+        super().__init__(matrix, font, fontsize, scaling, rise, text, textwidth, textdisp, ncs, graphicstate)
+        self.font = font
+        self.fontsize = fontsize
+        self.textwidth = textwidth
 
 
-##  LTFigure
-##
-class LTFigure(LTLayoutContainer):
-
-    def __init__(self, name, bbox, matrix):
-        self.name = name
-        self.matrix = matrix
-        (x, y, w, h) = bbox
-        bbox = get_bound(apply_matrix_pt(matrix, (p, q))
-                         for (p, q) in ((x, y), (x+w, y), (x, y+h), (x+w, y+h)))
-        LTLayoutContainer.__init__(self, bbox)
-        return
-
-    def __repr__(self):
-        return ('<%s(%s) %s matrix=%s>' %
-                (self.__class__.__name__, self.name,
-                 bbox2str(self.bbox), matrix2str(self.matrix)))
-
-    def analyze(self, laparams):
-        if not laparams.all_texts:
-            return
-        LTLayoutContainer.analyze(self, laparams)
-        return
-
-
-##  LTPage
-##
-class LTPage(LTLayoutContainer):
+class LTPageExtended(LTLayoutContainerExtended):
 
     def __init__(self, pageid, bbox, rotate=0):
-        LTLayoutContainer.__init__(self, bbox)
+        LTLayoutContainerExtended.__init__(self, bbox)
         self.pageid = pageid
         self.rotate = rotate
         return
@@ -254,6 +104,19 @@ class LTPage(LTLayoutContainer):
         return ('<%s(%r) %s rotate=%r>' %
                 (self.__class__.__name__, self.pageid,
                  bbox2str(self.bbox), self.rotate))
+
+
+class LTTextLineHorizontalExtended(LTTextLineHorizontal):
+    def __init__(self, word_margin):
+        super().__init__(word_margin)
+        self.fontsize = 0
+        self.font = None
+
+    def add(self, obj):
+        super().add(obj)
+        if len(self._objs) == 1:
+            self.font = self._objs[0].font
+            self.fontsize = self._objs[0].fontsize
 
 
 class LTPageHeader(LTTextContainer):
@@ -279,11 +142,14 @@ class LTPageHeader(LTTextContainer):
 class LTPageFooter(LTTextContainer):
     pass
 
+
 class LTSectionHeader(LTTextContainer):
     pass
 
+
 class LTSectionBody(LTTextContainer):
     pass
+
 
 class LTCitation(LTTextContainer):
     def __init__(self):
@@ -295,12 +161,13 @@ class LTCitation(LTTextContainer):
         self.date = '0000'
         self.link = ''
         return
+
     pass
+
 
 class LTEquation(LTTextContainer):
     pass
 
+
 class LTCaption(LTTextContainer):
     pass
-
-
